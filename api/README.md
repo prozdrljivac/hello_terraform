79e91491183f46b86941813c595f8b1673dea9ed
Petar Cevriz <pcevriz95@gmail.com>
2025-04-20 10:23:31 +0200

    Add postgres container, load envs, create initial connection to the DB

--- api/README.md
+++ api/README.md
@@ -2,9 +2,9 @@
 
 Todo:
 
+- Clean up current code
 - Implement Simple API for creating a message
 - Implement Simple API for getting list of messages
-- Implement Postgres DB
 - Implement Swagger
 - Implement Testcontainers
 
--- api/app.go
+++ api/app.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	ctx := context.Background()
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	fmt.Println(dsn)
+
+	dbpool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+	}
+	defer dbpool.Close()
+
+	var msg string
+	err = dbpool.QueryRow(ctx, "SELECT 'Hello, world!'").Scan(&msg)
+	if err != nil {
+		log.Fatalf("Query failed: %v\n", err)
+	}
+
+	fmt.Println("Message from DB:", msg)
+
 	serverPort := getEnvOrSetDefault("SERVER_PORT", "8080")
 
 	messageHandler := &MessageHandler{}
